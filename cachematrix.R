40105ecd695b892de42877ae17da8f0f9d1cb7ca
April Reeve <aprilreeve@sprintmail.com>
2015-02-22 16:57:58 -0500

    Programming Assignment: cacheMatrix.R; example: cacheMean.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Programming Assignment 2
+## Caching data outside current environment
 
-## Write a short comment describing this function
+## Creates a list of functions to set and get a matrix and the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    i <- NULL  ## reset to null
+    set <- function(y) {
+        x <<- y                ## set cached variable
+        i <<- NULL             ## indicates the inverse needs to be recalculated 
+    }
+    get <- function() x        ## get retrieves cached value of matrix
+    setinverse <- function(solve) i <<- solve   ## calculates  inverse
+    getinverse <- function() i   ## retrieves cached value of inverse
+    ## return list of functions to get and set matrix, and get and set inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function returns inverse of a matrix
+## if the inverse has already been cached then returns cache
+## must pass in list created by MakeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## check that matricx is invertable
+        if (det(x$get()) == 0) {
+            message("error - matrix is not invertable")
+            return()
+            }
+        ## check cache
+        i <- x$getinverse()
+        if(!is.null(i)) {  ## if something already cached just return that
+            message("getting cached data")
+            return(i)
+        }
+        ## place the cached matrix in data
+        data <- x$get()
+        ## calculate the inverse and place in i
+        i <- solve(data, ...)
+        ## store the calculated inverse in cache
+        x$setinverse(i)
+        ## Return the inverse matrix
+        i
 }
---
+++ cachemean.R
@@ -0,0 +1,33 @@
+## R Programming - Programming Assignment 2
+## Caching data outside current environment
+
+## Creates a list of functions to set and get a vector and the mean of the vector
+
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
+
+
+## Calculates the mean of a vector (unless already calculated)
+
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
